fdedf5ad68d9ad92495a0080bbe1dce0b5f1d8d8
IgorOleniuk <igor.oleniuk1@gmail.com>
2020-01-14 22:09:40 +0200

    fetch

--- pages/SomeData.vue
+++ pages/SomeData.vue
@@ -22,7 +22,7 @@ export default {
   },
 
   asyncData () {
-    return axios.get('https://jsonplaceholder.typicode.com/posts')
+    return fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => {
         const names = response.data
         return { names }
